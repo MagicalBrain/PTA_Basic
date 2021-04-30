5649846f02f5739a61ccaf1bba51ba3a03cadac2
MagicalBrain <13542593649@163.com>
2021-04-30 14:20:56 +0800

    update 1050

--- resource_code/1050.c
+++ resource_code/1050.c
@@ -12,19 +12,33 @@ int cmp(const void* A, const void* B) {
 }
 
 int main() {
-    int N;
+    int N, m, n;
     int *data;
     scanf("%d", &N);
     data = (int*) malloc(N * sizeof(int));
-    for (int i = 0; i < N; i++)
-        scanf("%d", &data[i]);
+    //for (int i = 0; i < N; i++)
+      //  scanf("%d", &data[i]);
     
     //排序：
     qsort(data, N, sizeof(int), cmp);
 
     //寻找符合题意的m和n
+    int i = 1;
+    for (; i * i < N; i++) ;
+    m = n = i;
+    printf("%d %d\n", m, n);
+
+    while (m * n != N)
+    {
+        if (m * n > N)
+            n--;
+        else m++;
+        if (m <= n)
+            m++;
+    }
+    printf("%d %d\n", m, n);
 
     //填充数进矩阵
-    
+
     return 0;
 }
